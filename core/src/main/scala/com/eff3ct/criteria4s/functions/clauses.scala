56e1714f57b4bb7b5ea25683e634a5cf41f3dade
Rafael Fernandez <github@rafaelfernandez.dev>
2026-03-24 17:03:32 +0100

    chore: update MIT license headers to 2024-2026

--- core/src/main/scala/com/eff3ct/criteria4s/functions/clauses.scala
+++ core/src/main/scala/com/eff3ct/criteria4s/functions/clauses.scala
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024 Rafael Fernandez
+ * Copyright (c) 2024-2026 Rafael Fernandez
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
